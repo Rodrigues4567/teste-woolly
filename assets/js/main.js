8503ffe213d00f30778091ff6046c53aa81a4a90
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2025-03-30 00:42:10 -0300

    Video section mobile and desktop version, done!

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,19 @@ window.addEventListener("load", function () {
         preloader.style.display = "none";
     }, 500);
 });
+
+// VIDEO PLAYER
+document.getElementById("playButton").addEventListener("click", function () {
+    let videoWrapper = document.getElementById("videoWrapper");
+
+    let videoEmbed = `
+        <iframe width="100%" height="100%"
+            src="https://www.youtube.com/embed/t9-cMiEDNyk?autoplay=1&rel=0"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+    `;
+
+    videoWrapper.innerHTML = videoEmbed;
+});
