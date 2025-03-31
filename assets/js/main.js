eabeaf993fd29e3fefb6cefee4c1aac0f6cfec3b
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2025-03-31 03:41:08 -0300

    functionalities done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
 
-// PRELOADER
-window.addEventListener("load", function () {
-    let preloader = document.getElementById("preloader");
+$(document).ready(function () {
+    // Máscara para CNPJ
+    $('#cnpj').mask('00.000.000/0000-00');
+    // Máscara para Telefone
+    $('#phone').mask('(00) 00000-0000');
 
-    preloader.style.opacity = "0";
-    setTimeout(() => {
-        preloader.style.display = "none";
-    }, 500);
-});
+    // PRELOADER
+    window.addEventListener("load", function () {
+        let preloader = document.getElementById("preloader");
 
-// VIDEO PLAYER
-document.getElementById("playButton").addEventListener("click", function () {
-    let videoWrapper = document.getElementById("videoWrapper");
+        preloader.style.opacity = "0";
+        setTimeout(() => {
+            preloader.style.display = "none";
+        }, 500);
+    });
 
-    let videoEmbed = `
-        <iframe width="100%" height="100%"
-            src="https://www.youtube.com/embed/t9-cMiEDNyk?autoplay=1&rel=0"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen>
-        </iframe>
-    `;
+    // VIDEO PLAYER
+    document.getElementById("playButton").addEventListener("click", function () {
+        let videoWrapper = document.getElementById("videoWrapper");
 
-    videoWrapper.innerHTML = videoEmbed;
-});
+        let videoEmbed = `
+            <iframe width="100%" height="100%"
+                src="https://www.youtube.com/embed/t9-cMiEDNyk?autoplay=1&rel=0"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+        `;
+
+        videoWrapper.innerHTML = videoEmbed;
+    });
+})
